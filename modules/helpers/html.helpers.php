e4aab93d8c61d128120fb5989851e395f46b22f0
Frédéric Delorme <frederic.delorme@gmail.com>
2010-08-23 23:19:48 +0200

    Add some Icons about buttons.

--- modules/helpers/html.helpers.php
+++ modules/helpers/html.helpers.php
@@ -6,11 +6,26 @@
  * @param string $title
  */
 function __link($path,$label,$title="",$options=null){
+	$accesskey="";
+	$accessflag=false;
+	if(strpos($label,'_')!==false){
+		$pos = strpos($label,'_');
+		$accesskey=$label[$pos+1];
+	}
 	$attributes="";
 	if($options!=null){
 		foreach($options as $key=>$value){
-			$attributes .= "$key=\"$value\""; 
+			if($key=="accesskey"){
+				$accessflag=true;
+			}
+			$attributes .= " $key=\"$value\""; 
 		}
-	} 
+	}
+	//echo "$pos:$accesskey/$accessflag"; 
+	if(!$accessflag && $accesskey!=""){
+		$label = str_replace('_','',$label);
+		$label = str_replace($accesskey,"<u>$accesskey</u>",$label);
+		$attributes .= " accesskey=\"$accesskey\""; 
+	}
 	echo "<a href=\"".__getRewiteMode().$path."\"".($title!=""?" title=\"$title\"":"")." $attributes>$label</a>";
 }
